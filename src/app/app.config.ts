ec1453ba5f59ce4f35b94db7b8e6115fbc4a4abd
Vladimir Perfilev <persoff68@gmail.com>
2018-08-03 18:00:28 +0300

    some corrections after moving

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import * as L from 'leaflet';
 
 export class AppConfig {
   public static get baseURL(): string {
-    return 'http://localhost:33380';
+    return 'http://93.90.221.10:33380';
   }
 
   public static get mainMenu() {
